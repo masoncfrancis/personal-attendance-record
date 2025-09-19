5a34bd61d6c316fd07d54268473c72160bdce985
Mason <masoncfrancis@gmail.com>
2025-09-19 12:04:46 -0400

    Revert "Revert "Update version number""
    This reverts commit 492c73b65fc6229bd8fe2772190576e687cb45dc.

--- cmd/par/main.go
+++ cmd/par/main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-const version = "0.1.0"
+const version = "0.1.1" 
 
 func main() {
 	// Get the absolute path to the directory containing the executable.
